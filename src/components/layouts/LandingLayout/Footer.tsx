a6fe1ea46e68ed413d847427166c177d36b48923
Infamous0192 <infamous0192@gmail.com>
2022-04-01 04:04:16 +0800

    Minor fix on footer

--- src/components/layouts/LandingLayout/Footer.tsx
+++ src/components/layouts/LandingLayout/Footer.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import logo from 'assets/logo.svg'
 
 const Footer: React.FC = () => {
   return (
@@ -11,29 +12,7 @@ const Footer: React.FC = () => {
             <div className="mb-2">
               {/* Logo */}
               <Link to="/" className="inline-block" aria-label="Cruip">
-                <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                  <defs>
-                    <radialGradient
-                      cx="21.152%"
-                      cy="86.063%"
-                      fx="21.152%"
-                      fy="86.063%"
-                      r="79.941%"
-                      id="footer-logo"
-                    >
-                      <stop stopColor="#4FD1C5" offset="0%" />
-                      <stop stopColor="#81E6D9" offset="25.871%" />
-                      <stop stopColor="#338CF5" offset="100%" />
-                    </radialGradient>
-                  </defs>
-                  <rect
-                    width="32"
-                    height="32"
-                    rx="16"
-                    fill="url(#footer-logo)"
-                    fillRule="nonzero"
-                  />
-                </svg>
+                <img src={logo} className="w-10 h-10" alt="logo tarkiz" />
               </Link>
             </div>
             <div className="text-sm text-gray-600">
