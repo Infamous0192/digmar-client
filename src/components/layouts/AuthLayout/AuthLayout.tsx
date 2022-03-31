f25adf5c14333ad109c555ea65bb602c2645ee07
Infamous0192 <infamous0192@gmail.com>
2022-04-01 01:42:59 +0800

    Minor improve on navbar

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,6 +1,8 @@
-import { useAuth } from 'hooks'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from 'hooks'
+
+import logo from 'assets/logo.svg'
 
 const AuthLayout: React.FC = ({ children }) => {
   const { state } = useAuth()
@@ -14,8 +16,13 @@ const AuthLayout: React.FC = ({ children }) => {
 
   return (
     <div className="w-full h-screen lg:grid grid-cols-2">
+      <div className="fixed top-[12px] left-4">
+        <Link to="/">
+          <img src={logo} className="w-10 h-10" alt="Logo Tarkiz Paz Banua" />
+        </Link>
+      </div>
       <div className="bg-white h-screen w-full flex items-center justify-center">{children}</div>
-      <div className="bg-gray-700 h-screen w-full hidden lg:block relative">
+      <div className="bg-gray-200 h-screen w-full hidden lg:block relative">
         <img
           src="https://images.unsplash.com/photo-1640622303392-7d2bee0c2438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           alt=""
--- src/components/navigation/Navbar/Navbar.tsx
+++ src/components/navigation/Navbar/Navbar.tsx
@@ -5,6 +5,8 @@ import { useAuth } from 'hooks'
 import { Button } from 'components/elements'
 import { Link } from 'react-router-dom'
 
+import logo from 'assets/logo.svg'
+
 const user = {
   name: 'Dwa Meizadewa',
   email: 'infamous0192@gmail.com',
@@ -50,12 +52,8 @@ const Navbar: React.FC = () => {
           <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-16">
               <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-8 w-8"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
-                  />
+                <div className="flex-shrink-0 -pr-4">
+                  <img className="h-10 w-10" src={logo} alt="Workflow" />
                 </div>
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4">
