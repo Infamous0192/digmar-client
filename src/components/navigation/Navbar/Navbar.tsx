fb8f2aff781960bbd069a661d16e75e931b483f1
Infamous0192 <infamous0192@gmail.com>
2022-04-26 10:14:33 +0800

    Add dashbard link up

--- src/components/navigation/Navbar/Navbar.tsx
+++ src/components/navigation/Navbar/Navbar.tsx
@@ -106,12 +106,12 @@ const Navbar: React.FC = () => {
                             <Menu.Item>
                               {({ active }) => (
                                 <Link
-                                  to="/profile"
+                                  to="/account/dashboard"
                                   className={`${
                                     active && 'bg-gray-100'
                                   } block px-4 py-2 text-sm text-gray-900`}
                                 >
-                                  Your Profile
+                                  Dashboard
                                 </Link>
                               )}
                             </Menu.Item>
@@ -239,10 +239,10 @@ const Navbar: React.FC = () => {
                     <div className="mt-3 px-2 space-y-1">
                       <Disclosure.Button
                         as={Link}
-                        to="/profile"
+                        to="/account/dashboard"
                         className="text-gray-600 text-sm hover:text-gray-900 hover:bg-gray-100 rounded block px-3 py-2"
                       >
-                        Your Profile
+                        Dashboard
                       </Disclosure.Button>
                       <Disclosure.Button
                         as={Link}
