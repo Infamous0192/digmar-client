f81ded9890df7ad8abccb3d68151a3447cafc4e0
Infamous0192 <infamous0192@gmail.com>
2022-05-15 04:41:05 +0800

    Create user preferrences

--- src/pages/member/index.tsx
+++ src/pages/member/index.tsx
@@ -4,7 +4,60 @@ import { DashboardLayout } from 'layouts/dashboard'
 const Dashboard: NextPage = () => {
   return (
     <DashboardLayout>
-      <div>awdlwauifgweualygvblerif</div>
+      <h1 className="text-2xl font-bold text-slate-800 leading-none mb-8">Progress Kelas</h1>
+      <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-x-10 gap-y-6">
+        <div className="bg-white rounded-lg shadow flex">
+          <div className="rounded-lg rounded-r-none h-full aspect-square bg-slate-200 flex-shrink-0"></div>
+          <div className="p-4 flex-grow">
+            <div className="flex items-center mb-4">
+              <div className="flex-grow">
+                <h2 className="text-lg font-semibold leading-none mb-2">
+                  Mencoba Hal Mudah : Volume I
+                </h2>
+                <p className="text-slate-600">Kelas Akuntansi</p>
+              </div>
+              <div className="flex-shrink-0 text-slate-600">12/12</div>
+            </div>
+            <div className="w-full h-2.5 bg-slate-200 rounded-full overflow-hidden">
+              <div className="w-full h-full bg-sunglow-500"></div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow flex">
+          <div className="rounded-lg rounded-r-none h-full aspect-square bg-slate-200 flex-shrink-0"></div>
+          <div className="p-4 flex-grow">
+            <div className="flex items-center mb-4">
+              <div className="flex-grow">
+                <h2 className="text-lg font-semibold leading-none mb-2">
+                  Mencoba Hal Mudah : Volume II
+                </h2>
+                <p className="text-slate-600">Kelas Akuntansi</p>
+              </div>
+              <div className="flex-shrink-0 text-slate-600">7/12</div>
+            </div>
+            <div className="w-full h-2.5 bg-slate-200 rounded-full overflow-hidden">
+              <div className="w-7/12 h-full bg-sunglow-500"></div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white rounded-lg shadow flex">
+          <div className="rounded-lg rounded-r-none h-full aspect-square bg-slate-200 flex-shrink-0"></div>
+          <div className="p-4 flex-grow">
+            <div className="flex items-center mb-4">
+              <div className="flex-grow">
+                <h2 className="text-lg font-semibold leading-none mb-2">
+                  Mencoba Hal Mudah : Volume III
+                </h2>
+                <p className="text-slate-600">Kelas Akuntansi</p>
+              </div>
+              <div className="flex-shrink-0 text-slate-600">1/7</div>
+            </div>
+            <div className="w-full h-2.5 bg-slate-200 rounded-full overflow-hidden">
+              <div className="w-1/12 h-full bg-sunglow-500"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </DashboardLayout>
   )
 }
--- src/pages/member/settings/index.tsx
+++ src/pages/member/settings/index.tsx
@@ -1,10 +1,44 @@
 import type { NextPage } from 'next'
 import { DashboardLayout } from 'layouts/dashboard'
+import { Button, Card } from 'components/elements'
+import { TextField } from 'components/forms'
 
 const Settings: NextPage = () => {
   return (
     <DashboardLayout>
-      <div>awdlwauifgweualygvblerif</div>
+      <Card>
+        <div className="p-6 md:p-8">
+          <h1 className="text-2xl font-bold text-slate-800 leading-none mb-6">My Account</h1>
+          <section className="mb-8">
+            <h2 className="text-xl font-bold text-slate-800 leading-none mb-2">Account Profile</h2>
+            <p className="mb-2">Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            <div className="flex flex-wrap md:flex-nowrap w-full gap-x-6 gap-y-2">
+              <div className="w-full">
+                <TextField type="text" name="name" label="Nama" value="Dwa Meizadewa" />
+              </div>
+              <div className="w-full">
+                <TextField type="text" name="nohp" label="No. HP" value="089612347584" />
+              </div>
+              <div className="w-full">
+                <TextField type="email" name="email" label="Email" value="infamous0192@gmail.com" />
+              </div>
+            </div>
+          </section>
+          <section>
+            <h2 className="text-xl font-bold text-slate-800 leading-none mb-2">Password</h2>
+            <p className="mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing.</p>
+            <Button color="light" className="!text-sunglow-600">
+              Set New Password
+            </Button>
+          </section>
+        </div>
+        <Card.Footer>
+          <div className="flex justify-end">
+            <Button color="light">Cancel</Button>
+            <Button className="ml-4">Save Changes</Button>
+          </div>
+        </Card.Footer>
+      </Card>
     </DashboardLayout>
   )
 }
